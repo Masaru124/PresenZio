f0ffae0e9471259517483d31d5dd04434e06f9bd
Masaru124 <masarukaze041@gmail.com>
2025-05-20 19:31:26 +0530

    deployment1

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
